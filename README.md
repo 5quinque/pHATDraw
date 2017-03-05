d8430ab8e3a96461f51c861c7b63c90fb1923755
linnit <ryan.linnit@outlook.com>
2017-03-05 18:35:45 +0000

    Cleaned stuff up

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # pHATDraw
+
+`sudo visudo`
+
+www-data ALL= (root) NOPASSWD: /var/www/html/led.py
+
--- led.py
+++ led.py
@@ -18,32 +18,7 @@ for d in data:
 	print data[d]
 	scrollphat.set_pixel(data[d][0], data[d][1], True)
 
-#for d in data:
-#	scrollphat.set_pixel(d[0], d[1], True)
-
 scrollphat.update()
 
 sys.exit(0)
-
-#x = int(sys.argv[1])
-#y = int(sys.argv[2])
-
-#print x, y
-
-if y <= 4 and x <= 10:
-	print("\nWe good");
-
-scrollphat.set_brightness(2)
-
-print scrollphat.buffer_len()
-
-#scrollphat.set_pixel(0,0, True)
-scrollphat.set_pixel(x,y, True)
-scrollphat.update()
-
-#for y in range(5):
-#	for x in range(11):
-#		scrollphat.set_pixel(x,y, True)
-#		scrollphat.update()
-#		time.sleep(0)
 
